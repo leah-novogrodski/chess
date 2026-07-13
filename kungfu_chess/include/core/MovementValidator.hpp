e80ee4f67aaecc5ef24118b10587863e27117633
Leah Novogrodski <l0556736612@gmail.com>
2026-07-13 09:54:42 +0300

    Delet : the last design

--- kungfu_chess/include/core/MovementValidator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Board.hpp"
-
-namespace core {
-    class MovementValidator {
-    public:
-        static bool isValidMove(const Board& board, int fromRow, int fromCol, int toRow, int toCol);
-    };
-}
