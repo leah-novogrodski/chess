9cc995df5941da3bd05b0cca35c20dd74b2ffa74
Leah Novogrodski <l0556736612@gmail.com>
2026-07-21 09:31:35 +0300

    add sound library

---
+++ audio/SoundLibrary.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace audio {
+namespace SoundLibrary {
+
+    inline const char* MOVE_SOUND = "assets/sound/click.wav";
+    inline const char* CAPTURE_SOUND = "assets/sound/eat.wav";
+    inline const char* GAME_OVER_SOUND = "assets/sound/game over.wav";
+
+}
+}
